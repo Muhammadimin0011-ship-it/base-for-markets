9ed363209c626b52afb348150dc400baa7b5e9ca
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-06-27 16:29:48 +0500

    style.css

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const contact = document.getElementById("result")
 const w = document.getElementById("loader")
+const nameInput = document.getElementById("InputName")
+const durationInput = document.getElementById("inputDuration")
+const dateInput = document.getElementById("inputDate")
+
+
+
+const modal = document.getElementById("modal");
+
+const open = document.querySelector(".search-btn").onclick = () => {
+    modal.classList.add("active");
+}
+
+const p = document.getElementById("closeModal").onclick = () => {
+    modal.classList.remove("active");
+}
+
+const close = document.getElementById("cancelBtn").onclick = () => {
+    modal.classList.remove("active");
+}
+
+window.onclick = (e) => {
+    if (e.target === modal) {
+        modal.classList.remove("active");
+    }
+}
 
 function get() {
     w.style.display = "flex"
@@ -23,40 +48,61 @@ function get() {
                     <hr class="divider"/>
                     <div class="actions">
                         <button class="btn btn-done"><i class="ti ti-check"></i></button>
-                        <button class="btn btn-edit"><i class="ti ti-edit"></i> Edit</button>
+                        <button onclick="edit(${item.id})" class="btn btn-edit"><i class="ti ti-edit"></i>Edit...</button>
                         <button class="btn btn-del" onclick="Delete(${item.id})"><i class="ti ti-trash"></i></button>
                     </div>
                 </div>`
             })
         })
         .finally(() => {
-    w.style.display = "none"
-})
+            w.style.display = "none"
+        })
 }
 
 get()
 
 function add() {
-    const inputname = document.getElementById("inputname")
-    const inputemail = document.getElementById("inputemail")
-    const inputphone = document.getElementById("inputphone")
 
     const values = {
-        name: inputname.value,
-        email: inputemail.value,
-        phone: inputphone.value
+
+        name: document.getElementById("inputName").value,
+
+        category: document.getElementById("inputCategory").value,
+
+        difficulty: document.getElementById("inputDifficulty").value,
+
+        duration: document.getElementById("inputDuration").value,
+
+        date: document.getElementById("inputDate").value,
+
+        notes: document.getElementById("inputNotes").value
+
     }
 
-    fetch('https://6a3b6583e4a07f202e14e54b.mockapi.io/workouts', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+    fetch("https://6a3b6583e4a07f202e14e54b.mockapi.io/workouts", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
         body: JSON.stringify(values)
+
     })
+
         .then(res => res.json())
+
         .then(data => {
+
             console.log(data)
-            location.reload()
+
+            modal.classList.remove("active")
+
+            get()
+
         })
+
 }
 
 function Delete(id) {
@@ -67,5 +113,18 @@ function Delete(id) {
         .then(data => {
             console.log(data)
             location.reload()
+        })
+}
+
+
+
+
+function edit(id) {
+    fetch(`https://6a3b6583e4a07f202e14e54b.mockapi.io/workouts/${id}`)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            modal.classList.add("active")
+            nameInput.value = res.name;
         })
 }
